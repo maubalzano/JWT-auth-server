23986ea49c50f217a63729ad57e74802a186f753
MAURIZIO <maurizio@iMac-di-Apple.wind3.hub>
2022-01-30 18:52:51 +0100

    fix something

--- server.js
+++ server.js
@@ -10,28 +10,28 @@ const bcrypt = require('bcrypt')
 app.use(cors())
 app.use(express.json());
 
-
-const accessTokenExpireTime = '20s'
 let users = [];
-let posts = 'Informazioni segrete importantissime porcodio';
+let posts = ['Dio è una merda', 'La Madonna è una bagascia', 'Dio e cane sono sinonimi', 'Mannaggia Gesù mannaggia'];
+let infos = ['Simone Russo', 'Serena Russo'];
 let refreshTokens = [];
-
 
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
 app.get('/posts', authenticateToken, (req, res) => {
-    console.log(req);
     res.json(posts)
+})
+
+app.get('/infos', authenticateToken, (req, res) => {
+    res.json(infos)
 })
 
 app.post('/users', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        console.log(salt);
-        console.log(hashedPassword);
+
         const user = {username: req.body.username, password: hashedPassword}
         users = [...users, user];
         res.status(201).send()
@@ -50,23 +50,29 @@ app.post('/users/token', (req, res) => {
         res.json({accessToken:accessToken})
     })
 })
-
 app.post('/users/login', async (req,res) => {
-    const user = users.find(user => user.username = req.body.username);
-    !user && res.status(400).send(`Cannot find user with username ${req.body.username}`)
-    try {
-        if(await bcrypt.compare(req.body.password, user.password)) {
 
-            const accessToken = generateAcessToken(user);
-            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-            refreshTokens = [...refreshTokens, refreshToken];
-            res.json({accessToken: accessToken, refreshToken: refreshToken})
-        } else {
-            res.send('Not allowed')
-        }
+    const user = users.find(user => user.username == req.body.username);
+    console.log(user)
+    if (!user) {
+        res.status(400).send(`Cannot find user with username ${req.body.username}`)
+    } else {
+        
+            try {
+                if(await bcrypt.compare(req.body.password, user.password)) {
+        
+                    const accessToken = generateAccessToken(user);
+                    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+                    refreshTokens = [...refreshTokens, refreshToken];
+                    res.json({accessToken: accessToken, refreshToken: refreshToken})
+                } else {
+                    res.send('Not allowed')
+                }
+        
+            } catch {
+                res.status(500).send()
+            }
 
-    } catch {
-        res.status(500).send()
     }
 })
 
@@ -81,14 +87,14 @@ function authenticateToken(req, res, next) {
     if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
+        if (err) return res.sendStatus(401)
         req.user = user
         next()
     })
 }
 
-function generateAcessToken(user) {
-    return jwt.sign({username: user.username}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: accessTokenExpireTime})
+function generateAccessToken(user) {
+    return jwt.sign({username: user.username}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'})
 }
 
 app.get('/login')
